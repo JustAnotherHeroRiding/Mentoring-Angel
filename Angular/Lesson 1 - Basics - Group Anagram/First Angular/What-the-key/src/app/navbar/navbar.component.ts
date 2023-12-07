51facde03c341f4dc5943752122c2f843797baad
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-12-07 15:54:56 +0100

    separated hamburger from grid portion

--- Angular/Lesson 1 - Basics - Group Anagram/First Angular/What-the-key/src/app/navbar/navbar.component.ts
+++ Angular/Lesson 1 - Basics - Group Anagram/First Angular/What-the-key/src/app/navbar/navbar.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  isSmallScreen = false;
+  menuActive = false;
 
+  constructor() {
+    // Optional: Check screen size on load
+    this.isSmallScreen = window.innerWidth < 630;
+    window.onresize = () => {
+      this.isSmallScreen = window.innerWidth < 630;
+    };
+  }
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
 }
