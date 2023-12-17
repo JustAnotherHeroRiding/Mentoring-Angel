1bad5de0cfb86c22888e34cb183d898af749b71a
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-12-17 20:21:25 +0100

    displaying multiple weather results plus location

--- Angular/Lesson 2 - Personal Dashboard/Personal-Dashboard/src/app/components/widgets/weather-widget/weather-widget.component.ts
+++ Angular/Lesson 2 - Personal Dashboard/Personal-Dashboard/src/app/components/widgets/weather-widget/weather-widget.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherData, WeatherService } from 'src/app/services/weather.service';
+interface WeatherWithCity extends WeatherData {
+  location: string;
+}
 @Component({
   selector: 'app-weather-widget',
   templateUrl: './weather-widget.component.html',
   styleUrls: ['./weather-widget.component.scss'],
 })
 export class WeatherWidgetComponent {
-  weatherData?: WeatherData;
-  location = ''
+  weatherData: WeatherWithCity[] = [];
+  location = '';
 
   constructor(private weatherService: WeatherService) {}
 
   getWeather(location: string) {
     this.weatherService.getWeather(location).subscribe({
-      next: (data) => (this.weatherData = data),
+      next: (data) => {
+        const extendedData = { ...data, location };
+        console.log(extendedData);
+        this.weatherData.unshift(extendedData);
+      },
       error: (err) => console.error(err),
-      complete: () => console.log(this.weatherData),
     });
   }
 }
