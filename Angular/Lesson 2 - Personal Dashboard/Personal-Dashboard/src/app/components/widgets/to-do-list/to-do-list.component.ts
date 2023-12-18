77a2d4cbff9fa582786dddc86a2f45524a394174
Kristijan Kocev <kristijankocev1234@gmail.com>
2023-12-18 20:17:54 +0100

    removing tasks

--- Angular/Lesson 2 - Personal Dashboard/Personal-Dashboard/src/app/components/widgets/to-do-list/to-do-list.component.ts
+++ Angular/Lesson 2 - Personal Dashboard/Personal-Dashboard/src/app/components/widgets/to-do-list/to-do-list.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.scss'],
 })
-export class ToDoListComponent {
+export class ToDoListComponent implements OnInit {
   task = '';
   tasks?: string[];
 
+  ngOnInit(): void {
+    this.tasks = localStorage.getItem('tasks')
+      ? JSON.parse(localStorage.getItem('tasks')!)
+      : [];
+  }
+
   addTask(task: string) {
+    localStorage.setItem(
+      'tasks',
+      JSON.stringify([...(this.tasks ?? []), task])
+    );
     this.tasks = [...(this.tasks ?? []), task];
     this.task = '';
+  }
+
+  removeTask(task: string) {
+    this.tasks = this.tasks?.filter((t) => t !== task);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
