7394661c544d629ae7c4dece7871ee4be6a3ad25
Kristijan Kocev <kristijankocev1234@gmail.com>
2024-01-31 17:10:46 +0100

    session check in the index page to display a welcome message

--- Angular/Lesson 4 - RxJs/chat-and-eat/src/app/pages/outlet/outlet.component.ts
+++ Angular/Lesson 4 - RxJs/chat-and-eat/src/app/pages/outlet/outlet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Session } from '@supabase/supabase-js';
+import { BehaviorSubject } from 'rxjs';
 import { SupabaseService } from 'src/app/Services/supabase.service';
 
 @Component({
@@ -11,12 +13,11 @@ export class OutletComponent implements OnInit {
   router = inject(Router);
   supabase = inject(SupabaseService);
   route = inject(ActivatedRoute);
+  session = new BehaviorSubject<Session | null | undefined>(undefined);
 
   ngOnInit(): void {
     this.supabase.session$.subscribe((session) => {
-      if (!session) {
-        this.router.navigate(['/auth']);
-      }
+      this.session.next(session);
     });
   }
 }
